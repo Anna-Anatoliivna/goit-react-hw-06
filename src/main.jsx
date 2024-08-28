b28d85550912b5f747a140ca8f7bc68abe63240b
Anna <iaanya27@gmail.com>
2024-08-28 10:42:05 +0300

    added store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { UserContexProvider } from './content/UserContex';
+import { Provider } from 'react-redux';
+import { store } from './store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <UserContexProvider>
+        <App />
+      </UserContexProvider>
+    </Provider>
+  </React.StrictMode>
+);
